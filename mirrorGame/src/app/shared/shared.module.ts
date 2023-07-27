8457ace72e7cd8eb71c68d0fe06de60a6ca17c8e
Abdelrahman Ahmed <ba9186792@gmail.com>
2023-07-27 23:18:04 +0300

    notfoundpage

---
+++ mirrorGame/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+
+
+
+@NgModule({
+  declarations: [
+    NotfoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
