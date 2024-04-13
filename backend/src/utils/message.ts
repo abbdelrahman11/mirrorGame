06ee73e3e615f083ef81cde347651ea19517194a
Abdelrahman Ahmed <ba9186792@gmail.com>
2024-04-13 09:47:37 +0200

    init commit

--- backend/src/utils/message.ts
+++
@@ -1,7 +0,0 @@
-import { messages, messagesModel } from "../models/messages.model";
-
-function formatMessage(msg: messages) {
-  return messagesModel.create(msg);
-}
-
-module.exports = { formatMessage };
